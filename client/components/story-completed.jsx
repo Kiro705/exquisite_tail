2bea23c27a8debf9a84219c5f9602f1eec5fbb59
Jackson Sui <kiro705@gmail.com>
2018-02-08 12:20:29 -0500

    FORCE=TRUE added css to single story views

--- client/components/story-completed.jsx
+++ client/components/story-completed.jsx
@@ -7,14 +7,14 @@ import SingleChapter from './single-chapter.jsx'
 export default function StoryCompleted(props){
 	const {story} = props
   return (
-		<div className='notification'>
-        <h2 className='inlineBlock'>The <i>tail</i> of {story.title}</h2>
+	<div>
+        <h2 className='storyTitle inlineBlock'>The <i>tail</i> of <u>{story.title}</u></h2>
         {
         	story.content.map(chapter => {
         		return(
-        			<div key={chapter.id}>
-        				<h4>Chapter {chapter.place}</h4>
-        				<h6>{chapter.content}</h6>
+        			<div key={chapter.id} className='chapterContainer'>
+        				<h4 className='font-color-light'>Chapter {chapter.place}</h4>
+        				<h5 className='padding-left-15'>{chapter.content}</h5>
         			</div>
         		)
         	})
--- client/components/story-in-progress.jsx
+++ client/components/story-in-progress.jsx
@@ -7,8 +7,8 @@ export default function StoryInProgress(props){
 	const {story} = props
   return (
 		<div className='storyView'>
-        <h3 className='inlineBlock'>The <i>tail</i> {story.title} is currently on chapter {story.currentChapter}/{story.chapterAmount}</h3>
-        <h5>It's {story.currentWriter}'s turn to write.</h5> 
+        <h3 className='inlineBlock'>The <i>tail</i> of {story.title} ({story.currentChapter}/{story.chapterAmount})</h3>
+        <h5>It's currently {story.currentWriter}'s turn to write.</h5> 
     </div>
   )
 }
